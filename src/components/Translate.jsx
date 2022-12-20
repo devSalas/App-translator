4cebf883b968f48c2094da42d2e6d79b030501e1
esteban salas <esalassulca@gmail.com>
2022-12-19 20:46:04 -0500

    feat: agregar fondo

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,13 +1,11 @@
 import { Route, Router, Routes } from "react-router-dom";
-import Home from "./Home";
 import TraduceAhora from "./TraduceAhora";
-import styled from "./Translate.module.css"
+
 
 const Translate = () => {
   return ( 
-    <div className={styled.container}>
+    <div>
       <Routes>
-        {/* <Route path="/" element={<Home/>}/> */}
         <Route path="/" element={<TraduceAhora/>}/>
       </Routes>
     </div>
